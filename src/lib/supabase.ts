2153be3a3cf7bfe2929a77a494eec87b4b0e167c
Kevin Payne <kevinpayne@macbookpro.mynetworksettings.com>
2025-12-24 18:24:55 -0500

    LuxExpose Astro SSR

---
+++ src/lib/supabase.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("SUPABASE_URL missing. Check .env file.");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("SUPABASE_ANON_KEY missing. Check .env file.");
+}
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
+
