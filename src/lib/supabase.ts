e99ba5962332b542712d2a0f1211b67115671f72
Kevin Payne <kevinpayne@macbookpro.mynetworksettings.com>
2025-12-24 18:56:01 -0500

    Use Astro env variables for Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,19 +1,17 @@
-import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl) {
-  throw new Error("SUPABASE_URL missing. Check .env file.");
+  throw new Error("SUPABASE_URL missing. Check Vercel Environment Variables.");
 }
 
 if (!supabaseAnonKey) {
-  throw new Error("SUPABASE_ANON_KEY missing. Check .env file.");
+  throw new Error("SUPABASE_ANON_KEY missing. Check Vercel Environment Variables.");
 }
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 );
-
